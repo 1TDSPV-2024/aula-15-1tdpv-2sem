ace31914d1e480b04efbfca2670cf2adb4caa543
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-10-04 22:56:04 -0300

    Implementando endpoint do tipo POST e testando com thunder.

--- aula15/src/app/api/base-produtos/route.tsx
+++ aula15/src/app/api/base-produtos/route.tsx
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server";
 import { promises as fs } from "fs";
 
+//ENDPOINT DESTE ARQUIVO:
+//http://localhost:3000/api/base-produtos
+
 export async function GET() {
 
     const file = await fs.readFile(process.cwd() + "/src/data/base.json", "utf-8");
@@ -8,3 +11,15 @@ export async function GET() {
     return NextResponse.json(dados);
 }
 
+export async function POST(request:Request) {
+
+    //A requisição do tipo POST recebe umobjeto request e deve
+    //retornar um objeto response, mas antes deve realizar o
+    // destructuring no objeto request e receber os dados que foram
+    // enviados no corpo da requisição.
+    const { nome, idade } = await request.json();
+    
+    const dados = { nome, idade };
+
+    return NextResponse.json({dados,msg:"SUCESSO"},{status:201});
+}
--- aula15/src/app/page.tsx
+++ aula15/src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div>
       <h1>Welcome to my home page</h1>
       <Image src="/img/mouse.jpg" alt="mouse" width={50} height={50} priority={true} />
+    
     </div>
   )
 }
